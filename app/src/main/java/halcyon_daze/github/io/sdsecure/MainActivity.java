31093c0a6a349b0ff67b9309361925bc045ce9a0
Christopher Chin <Halcyon-Days@users.noreply.github.com>
2018-03-12 17:05:08 -0700

    First test

--- app/src/main/java/halcyon_daze/github/io/sdsecure/MainActivity.java
+++ app/src/main/java/halcyon_daze/github/io/sdsecure/MainActivity.java
@@ -1,26 +1,114 @@
 package halcyon_daze.github.io.sdsecure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    final EditText idText = (EditText) findViewById(R.id.idText);
+    final EditText latText = (EditText) findViewById(R.id.latText);
+    final EditText lngText = (EditText) findViewById(R.id.lngText);
+    final EditText encryptText = (EditText) findViewById(R.id.encryptText);
+    final TextView serverResponseText = (TextView) findViewById(R.id.serverResponseText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText requestText = (EditText) findViewById(R.id.request);
-
-        Button searchBtn =  findViewById(R.id.sendRequestButton);
-        searchBtn.setOnClickListener(new View.OnClickListener() {
+        Button postBtn = findViewById(R.id.postBut);
+        postBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                requestText.setText("");
+                new asyncServerPost().execute(getApplicationContext() );
             }
         });
+
+        Button getBtn = findViewById(R.id.getBut);
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new asyncServerGet().execute(getApplicationContext() );
+            }
+        });
+
+        Button deleteBtn = findViewById(R.id.deleteBut);
+        deleteBtn .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new asyncServerDelete().execute(getApplicationContext() );
+            }
+        });
+    }
+
+    //asynchronous task to send request
+    private class asyncServerPost extends AsyncTask<Context, Void, String> {
+
+        protected String doInBackground(Context... input) {
+            String returnText = "";
+
+            HashMap<String, String> params = new HashMap<String, String>();
+            params.put("lat",latText.getText().toString());
+            params.put("lng",lngText.getText().toString());
+            params.put("encryption",encryptText.getText().toString());
+
+            returnText = ServerComm.getRequest(ServerComm.POST, params);
+
+            return returnText;
+        }
+
+        protected void onPostExecute(String returnText) {
+            //updates text boxes based on result of searching for stop
+            serverResponseText.setText("Sending a POST request: \n " + returnText);
+
+        }
+    }
+
+    //asynchronous task to send request
+    private class asyncServerGet extends AsyncTask<Context, Void, String> {
+
+        protected String doInBackground(Context... input) {
+            String returnText = "";
+
+            HashMap<String, String> params = new HashMap<String, String>();
+            params.put("id",idText.getText().toString());
+            returnText = ServerComm.getRequest(ServerComm.GET, params);
+
+            return returnText;
+        }
+
+        protected void onPostExecute(String returnText) {
+            //updates text boxes based on result of searching for stop
+            serverResponseText.setText("Sending a GET request: \n " + returnText);
+
+        }
+    }
+
+    //asynchronous task to send request
+    private class asyncServerDelete extends AsyncTask<Context, Void, String> {
+
+        protected String doInBackground(Context... input) {
+            String returnText = "";
+
+            HashMap<String, String> params = new HashMap<String, String>();
+            params.put("id",idText.getText().toString());
+            returnText = ServerComm.getRequest(ServerComm.DELETE, params);
+
+            return returnText;
+        }
+
+        protected void onPostExecute(String returnText) {
+            //updates text boxes based on result of searching for stop
+            serverResponseText.setText("Sending a DELETE request: \n " + returnText);
+
+        }
     }
 }
---
+++ app/src/main/java/halcyon_daze/github/io/sdsecure/ServerComm.java
@@ -0,0 +1,87 @@
+package halcyon_daze.github.io.sdsecure;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.HashMap;
+import java.net.URL;
+
+/**
+ * Created by Christopher on 2018-03-12.
+ */
+
+public class ServerComm {
+    public static final String POST = "POST";
+    public static final String DELETE = "DELETE";
+    public static final String GET = "GET";
+    private static final String URL = "http://pk080596pi.ddns.net:5000/history?";
+
+    /*
+     * Wrapper for send request, assuming url is constant
+     */
+    public static String getRequest (String requestType, HashMap<String, String> parameters){
+        return sendRequest(requestType, URL, parameters);
+    }
+
+    public static String sendRequest (String requestType, String url, HashMap<String, String> parameters){
+        String result = "";
+
+        try {
+            URL pageURL = new URL(addParams(url, parameters));
+
+            System.out.println("Connecting to " + pageURL);
+
+            HttpURLConnection urlConnection = (HttpURLConnection) pageURL.openConnection();
+            urlConnection.setRequestMethod(requestType);
+
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            result = convertStreamToString(in);
+
+        } catch (IOException e) {
+            result = "Request failed";
+        }
+
+        return result;
+    }
+
+    private static String convertStreamToString (InputStream in) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        StringBuilder sb = new StringBuilder();
+
+        String input;
+
+        try {
+            while ((input = reader.readLine()) != null) {
+                sb.append(input).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static String addParams(String url, HashMap<String, String> parameters) {
+        int i = 0;
+
+        for (String key : parameters.keySet()) {
+            url += key + "=" + parameters.get(key);
+
+            if (++i < parameters.keySet().size()) {
+                url += "&";
+            }
+        }
+
+        return url;
+    }
+}
